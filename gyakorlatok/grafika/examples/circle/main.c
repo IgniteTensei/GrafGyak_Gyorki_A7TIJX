021f20f9bf0d9b6d0d715226184c9dfcb3fc9f38
Györki Ádám <37386030+IgniteTensei@users.noreply.github.com>
2023-04-14 17:04:46 +0200

    Rendezés és Repo Frissítés

--- gyakorlatok/grafika/examples/circle/main.c
+++ gyakorlatok/grafika/examples/circle/main.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 	Circle circle;
 	double area;
 	
-	set_circle_data(&circle, 8, 13, 9);
+	set_circle_data(&circle, 5, 10, 8);
 	area = calc_circle_area(&circle);
 	
 	printf("Circle area: %lf\n", area);
